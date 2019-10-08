5b8aa75f611b2490ff0263323b786be55f1e2d72
Lucas de Almeida <almeidadelucas@gmail.com>
2019-10-08 08:20:54 -0300

    Finishing model and creating validation to fields

--- server/src/modules/ingridients.js
+++ server/src/modules/ingridients.js
@@ -1,7 +1,15 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
  
-const Ingridient = new Schema({
-  name: String,
-  value: Float32Array
+const IngridientSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, "Missing name!"]
+  },
+  value: {
+    type: Float32Array,
+    required: [true, "Missing value!"]
+  }
 });
+
+const Ingridient = mongoose.model('Ingridient', IngridientSchema);
