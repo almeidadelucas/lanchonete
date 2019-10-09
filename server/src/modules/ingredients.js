59139a973b9820e42b1c88ba27bb15eaca7616ea
Lucas de Almeida <almeidadelucas@gmail.com>
2019-10-09 08:13:28 -0300

    Creating post route and adding nodemon

--- server/src/modules/ingredients.js
+++ server/src/modules/ingredients.js
@@ -1,15 +1,13 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
  
-const IngridientSchema = new Schema({
+const IngredientSchema = new Schema({
   name: {
     type: String,
     required: [true, "Missing name!"]
   },
   value: {
-    type: Float32Array,
+    type: Number,
     required: [true, "Missing value!"]
   }
 });
-
-const Ingridient = mongoose.model('Ingridient', IngridientSchema);
--- server/src/routes/ingredients.js
+++ server/src/routes/ingredients.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const IngredientSchema = require('../modules/ingredients');
+
 const router = express.Router();
+const Ingredient = mongoose.model('Ingredient', IngredientSchema);
 
 router.get('/', (req, res) => {
-    return res.send('bla'); 
- });
+    Ingredient.find({'name': 'salada'}, 'name value', function (err, ingredients) {
+        err ? res.send('Error to find ingredients!') : res.send(ingredients);
+    });
+});
+
+router.post('/post/:name', (req, res) => {
+    res.send(req.params.name);
+});
 
 module.exports = router;
