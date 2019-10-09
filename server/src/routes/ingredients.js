e34ba6b1c78b5b96d38bf6973b2c9a0d8ff926a6
Lucas de Almeida <almeidadelucas@gmail.com>
2019-10-09 12:39:27 -0300

    Post method

--- server/src/routes/ingredients.js
+++ server/src/routes/ingredients.js
@@ -6,13 +6,16 @@ const router = express.Router();
 const Ingredient = mongoose.model('Ingredient', IngredientSchema);
 
 router.get('/', (req, res) => {
-    Ingredient.find({'name': 'salada'}, 'name value', function (err, ingredients) {
+    Ingredient.find({name: 'salada'}, 'name value', function (err, ingredients) {
         err ? res.send('Error to find ingredients!') : res.send(ingredients);
     });
 });
 
 router.post('/post/:name', (req, res) => {
-    res.send(req.params.name);
+    let newIngredient = new Ingredient({name: req.params.name, value: 2.5});
+    newIngredient.save(function(err) {
+        err ? res.send('Error to create ingredint!') : res.send('Ingredient created with succes!');
+    });
 });
 
 module.exports = router;
