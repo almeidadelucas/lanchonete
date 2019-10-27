41dd5c2cd3a538afcac15897ba5da4af1f2db7a9
Lucas de Almeida <almeidadelucas@gmail.com>
2019-10-27 20:19:20 -0300

    fix lint

--- server/src/routes/ingredients.js
+++ server/src/routes/ingredients.js
@@ -4,40 +4,24 @@ const Ingredient = require('../schema/ingredients.js');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    Ingredient.find((err, ingredients) => {
-        return err ? res.send('Error to find all ingredients!') : res.send(ingredients);
-    });
+  Ingredient.find((err, ingredients) => (err ? res.send('Error to find all ingredients!') : res.send(ingredients)));
 });
 
 router.get('/:id', (req, res) => {
-    Ingredient.findOne({_id: req.params.id}, 'name value', (err, ingredient) => {
-        return err ? res.send('Error to find ingredient!') : res.send(ingredient);
-    });
+  Ingredient.findOne({ _id: req.params.id }, 'name value', (err, ingredient) => (err ? res.send('Error to find ingredient!') : res.send(ingredient)));
 });
 
 router.post('/', (req, res) => {
-    const newIngredient = new Ingredient(req.body);
-    newIngredient.save(err => {
-        return err ? res.send('Error to create ingredient!') : res.send(newIngredient)
-    });
+  const newIngredient = new Ingredient(req.body);
+  newIngredient.save((err) => (err ? res.send('Error to create ingredient!') : res.send(newIngredient)));
 });
 
 router.delete('/', (req, res) => {
-    Ingredient.deleteMany(err => {
-        return err ? res.send('Error to delete all ingredients!') : res.send('Ingredients deleted with succes!');
-    });
+  Ingredient.deleteMany((err) => (err ? res.send('Error to delete all ingredients!') : res.send('Ingredients deleted with succes!')));
 });
 
 router.delete('/:id', (req, res) => {
-    Ingredient.deleteOne({_id: req.params.id}, err => {
-        return err ? res.send('Error to delete this ingredient!') : res.send('Ingredient deleted with succes!');
-    });
+  Ingredient.deleteOne({ _id: req.params.id }, (err) => (err ? res.send('Error to delete this ingredient!') : res.send('Ingredient deleted with succes!')));
 });
-
-// router.post('/:id', (req, res) => {
-//     Ingredient.updateOne(err => {
-//         return err ? res.send('Error to update ingredient!') : res.send('Ingredient updated with succes!');
-//     });
-// });
 
 module.exports = router;
