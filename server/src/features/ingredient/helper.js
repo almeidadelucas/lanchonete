2eac6d9c6f47bacd7c95d9cebd63410414595f45
Lucas de Almeida <almeidadelucas@gmail.com>
2020-02-08 16:54:59 -0300

    fix lint

--- server/src/features/ingredient/helper.js
+++ server/src/features/ingredient/helper.js
@@ -13,22 +13,29 @@ const create = (data) => {
       const newIngredient = new Ingredient(ingredient);
       newIngredient.save();
       savedIngredients.push(newIngredient);
-    }); 
+    });
     return Promise.resolve(savedIngredients);
   } catch (err) {
     return Promise.reject(err);
   }
 };
 
-const destroy = (id) => Ingredient.deleteOne({ _id: id }, (err) => (err ? Promise.settled(err) : Promise.resolve("Ingredient deleted with success!")));
+const destroy = (id) => Ingredient.deleteOne({ _id: id },
+  (err) => (err ? Promise.settled(err) : Promise.resolve('Ingredient deleted with success!')));
 
-const destroyAll = () => Ingredient.deleteMany((err) => (err ? Promise.settled(err) : Promise.resolve('Ingredients deleted with succes!')));
+const destroyAll = () => Ingredient.deleteMany(
+  (err) => (err ? Promise.settled(err) : Promise.resolve('Ingredients deleted with succes!')),
+);
 
-const findById = (id) => Ingredient.findOne({ _id: id }, 'name price', (err, data) => (err ? Promise.settled(err) : Promise.resolve(data)));
+const findById = (id) => Ingredient.findOne({ _id: id }, 'name price',
+  (err, data) => (err ? Promise.settled(err) : Promise.resolve(data)));
 
-const findAll = () => Ingredient.find((err, data) => (err ? Promise.settled(err) : Promise.resolve(data)));
+const findAll = () => Ingredient.find(
+  (err, data) => (err ? Promise.settled(err) : Promise.resolve(data)),
+);
 
-const update = (id, data) => Ingredient.findByIdAndUpdate(id, data, { new: true }, (err, data) => (err ? Promise.settled(err) : Promise.resolve(data)));
+const update = (id, newData) => Ingredient.findByIdAndUpdate(id, newData, { new: true },
+  (err, data) => (err ? Promise.settled(err) : Promise.resolve(data)));
 
 module.exports = {
   create,
--- server/src/features/ingredient/routes.js
+++ server/src/features/ingredient/routes.js
@@ -1,18 +1,15 @@
 const express = require('express');
 const helper = require('./helper');
-const Ingredient = require('./model');
 
 const router = express.Router();
 
 router.get('/', (req, res) => helper.findAll()
   .then((data) => res.status(200).send(data))
-  .catch((err) => res.status(500).send(err))
-);
+  .catch((err) => res.status(500).send(err)));
 
 router.get('/:id', (req, res) => helper.findById(req.params.id)
   .then((data) => res.status(200).send(data))
-  .catch((err) => res.status(500).send(err))
-);
+  .catch((err) => res.status(500).send(err)));
 
 router.post('/', (req, res) => helper.create(req.body)
   .then((data) => res.sendStatus(200).send(data))
@@ -20,17 +17,14 @@ router.post('/', (req, res) => helper.create(req.body)
 
 router.put('/:id', (req, res) => helper.update(req.params.id, req.body)
   .then((data) => res.status(200).send(data))
-  .catch((err) => res.status(500).send(err))
-);
+  .catch((err) => res.status(500).send(err)));
 
 router.delete('/', (req, res) => helper.destroyAll()
   .then((data) => res.status(200).send(data))
-  .catch((err) => res.status(500).send(err))
-);
+  .catch((err) => res.status(500).send(err)));
 
-router.delete('/:id', (req, res) =>  helper.destroy(req.params.id)
+router.delete('/:id', (req, res) => helper.destroy(req.params.id)
   .then((data) => res.status(200).send(data))
-  .catch((err) => res.status(500).send(err))
-);
+  .catch((err) => res.status(500).send(err)));
 
 module.exports = router;
