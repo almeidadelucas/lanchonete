2a519a73762d4b7c138ee3f60926795cb4480f9a
Lucas de Almeida <almeidadelucas@gmail.com>
2020-06-18 19:03:37 -0300

    removing non used variables

--- frontend/src/components/routers/index.jsx
+++ frontend/src/components/routers/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch, Redirect } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Layout from '../layout';
 import Home from '../../features/home';
 import Menu from '../../features/menu';
