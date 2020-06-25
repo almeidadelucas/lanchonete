e0c1b025934b24d414e5ca5ed62f879e4e7bf9c2
Lucas de Almeida <almeidadelucas@gmail.com>
2020-06-25 20:46:59 -0300

    Creating footer

--- frontend/src/components/footer/index.jsx
+++ frontend/src/components/footer/index.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import { Typography } from '@material-ui/core';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import logo from '../../_assets/img/logo.svg'
 import './index.css';
 
 const Footer = () => {
   return (
     <div className="footer">
-      <h2>This is my footer</h2>
+      <div className="footer-logo">
+        <img src={logo} alt="logo" />
+      </div>
+      <div className="footer-contacts">
+        <Typography variant="h5">
+          Redes Sociais
+        </Typography>
+        <div>
+          <FacebookIcon className="footer-social-media" />
+          <InstagramIcon className="footer-social-media" />
+          <TwitterIcon className="footer-social-media" />
+        </div>
+      </div>
     </div>
   );
 };
